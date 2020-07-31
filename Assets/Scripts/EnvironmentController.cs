beefa239553bd80bec70d036796d29c208a8dc38
Gabe Gomez <gag014@bucknell.edu>
2020-07-30 21:18:18 -0400

    Initial commit

---
+++ Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+    // To set the species that can be spawned
+    public GameObject[] species;
+    public Transform speciesHolder;
+
+    // Setting up the environments carrying capacity
+    public int carryingCapacity = 1000;
+
+    [Range(0.0f, 10.0f)]
+    public float species1FertilityRate = 2;
+    [Range(0.0f, 10.0f)]
+    public float species2FertilityRate = 2;
+
+    private MeshCollider _meshCollider;
+    private int _speciesSpawnAmount;
+    private float _spawnPauser = 0.001f;
+
+    private bool _gamePaused;
+
+    private float _xDim;
+    private float _zDim;
+
+    private Transform NEdge;
+    private Transform SEdge;
+    private Transform EEdge;
+    private Transform WEdge;
+
+    private ArrayList readyToDie; // List of game objects ready to die
+
+    private void Start()
+    {
+        _gamePaused = false;
+        _meshCollider = GetComponent<MeshCollider>();
+        readyToDie = new ArrayList();
+
+        _xDim = _meshCollider.bounds.size.x;
+        _zDim = _meshCollider.bounds.size.z;
+
+        this.NEdge = this.transform.Find("NEdge");
+        this.SEdge = this.transform.Find("SEdge");
+        this.EEdge = this.transform.Find("EEdge");
+        this.WEdge = this.transform.Find("WEdge");
+    }
+
+    private void Update()
+    {
+        if (speciesHolder.childCount >= carryingCapacity)
+        {
+            for (int i = 0; i < readyToDie.Count; i++)
+            {
+                GameObject spawn = (GameObject)readyToDie[i];
+
+                readyToDie.RemoveAt(i);
+                Destroy(spawn);
+            }
+        }
+    }
+
+    public void Setup()
+    {
+        if (carryingCapacity % 2 == 1)
+        {
+            carryingCapacity -= 1;
+        }
+
+        _speciesSpawnAmount = carryingCapacity / species.Length;
+
+        for (int i = 0; i < species.Length; i++)
+        {
+            StartCoroutine(SpawnSpecies(species[i]));
+        }
+    }
+
+    public void AddObjectReadyToDie(GameObject spawn)
+    {
+        readyToDie.Add(spawn);
+    }
+
+    IEnumerator SpawnSpecies(GameObject species)
+    {
+
+        for (int i = 0; i < _speciesSpawnAmount; i++)
+        {
+            float randomX = Random.Range(-_xDim/2, _xDim/2);
+            float randomZ = Random.Range(-_zDim/2, _zDim/2);
+
+            Vector3 randomPosition = new Vector3(randomX, _meshCollider.transform.position.y + 5, randomZ);
+
+            GameObject spawn = Instantiate(species, randomPosition, Quaternion.identity, speciesHolder);
+
+            spawn.GetComponent<SpeciesController>().SetEnvironment(_meshCollider, speciesHolder, this, NEdge, SEdge, EEdge, WEdge);
+
+            yield return new WaitForSeconds(_spawnPauser);
+        }
+    }
+
+    //IEnumerator SpeciesGeneration(float generationTime)
+    //{
+    //    while(!_gamePaused)
+    //    {
+    //        float randomX = Random.Range(-_boxCollider.size.x + 1, _boxCollider.size.x - 1);
+    //        float randomZ = Random.Range(-_boxCollider.size.z + 1, _boxCollider.size.z - 1);
+
+    //            Vector3 randomPosition = new Vector3(randomX, _boxCollider.size.y + _boxCollider.center.y, randomZ);
+
+    //          Instantiate(species, randomPosition, Quaternion.identity, speciesHolder);
+
+    //        yield return new WaitForSeconds(generationTime);
+    //  }
+    //}
+
+
+}
---
+++ Assets/Scripts/SpeciesController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeciesController : MonoBehaviour
+{
+    private EnvironmentController environmentController;
+    private MeshCollider _environmentCollider;
+    private BoxCollider _speciesCollider;
+    private Rigidbody _rb;
+    private bool _canMove;
+
+    private Transform NEdge;
+    private Transform SEdge;
+    private Transform EEdge;
+    private Transform WEdge;
+
+    public float speed = 20;
+    public float generationTime;
+    [Range(1,10)]
+    public float fertilityRate = 2.2f;
+    public float rotationEpsilon = 45;
+
+    private Transform speciesHolder;
+
+    private float currentTimeUntilBirth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeUntilBirth = 0;
+        _canMove = false;
+        _speciesCollider = GetComponent<BoxCollider>();
+        _rb = GetComponent<Rigidbody>();
+
+        Vector3 randomRotation = transform.eulerAngles;
+        randomRotation.y = Random.Range(0f, 360f);
+        transform.rotation = Quaternion.Euler(randomRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_rb.velocity.y == 0)
+        {
+            _canMove = true;
+        }
+
+        if (_canMove)
+        {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            Vector3 currentPosition = this.transform.position;
+            Vector3 nextPosition = currentPosition + this.transform.forward;
+
+            this.transform.position = Vector3.Lerp(currentPosition, nextPosition, step);
+            currentTimeUntilBirth += Time.deltaTime;
+
+            if (currentTimeUntilBirth >= generationTime)
+            {
+                environmentController.AddObjectReadyToDie(this.gameObject);
+                StartCoroutine(GiveBirth());
+                currentTimeUntilBirth -= generationTime;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (hit.distance <= 5)
+            {
+                Vector3 newRotation = transform.eulerAngles;
+
+                float turnedAroundY = Random.Range(-newRotation.y - rotationEpsilon, -newRotation.y + rotationEpsilon);
+
+                newRotation.y = turnedAroundY;
+
+                transform.rotation = Quaternion.Euler(newRotation);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+    }
+
+    IEnumerator GiveBirth()
+    {
+        float weightResult = Random.value;
+
+        float probablity = fertilityRate - Mathf.Floor(fertilityRate);
+
+        int birthAmount;
+
+        if (weightResult <= probablity)
+        {
+            birthAmount = (int) Mathf.Floor(fertilityRate);
+        }
+
+        else
+        {
+            birthAmount = (int)Mathf.Ceil(fertilityRate);
+        }
+
+        for (int i = 0; i < birthAmount; i++)
+        {
+            if (environmentController.speciesHolder.childCount < environmentController.carryingCapacity)
+            {
+                float _xDim = _environmentCollider.bounds.size.x;
+                float _zDim = _environmentCollider.bounds.size.z;
+
+                float randomX = Random.Range(-_xDim / 2, _xDim / 2);
+                float randomZ = Random.Range(-_zDim / 2, _zDim / 2);
+
+                Vector3 randomPosition = new Vector3(randomX, _environmentCollider.transform.position.y + 5, randomZ);
+
+                GameObject spawn = Instantiate(this.gameObject, randomPosition, Quaternion.identity, speciesHolder);
+
+                spawn.GetComponent<SpeciesController>().SetEnvironment(_environmentCollider, speciesHolder, environmentController, NEdge, SEdge, EEdge, WEdge);
+
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
+
+    public void SetEnvironment(MeshCollider meshCollider, Transform speciesHolder, EnvironmentController controller, Transform NEdge, Transform SEdge, Transform EEdge, Transform WEdge)
+    {
+        this.environmentController = controller;
+        _environmentCollider = meshCollider;
+        this.speciesHolder = speciesHolder;
+        this.NEdge = NEdge;
+        this.SEdge = SEdge;
+        this.WEdge = WEdge;
+        this.EEdge = EEdge;
+
+    }
+}
